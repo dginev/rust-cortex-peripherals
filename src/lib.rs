342f7448c821172db3f3ed6c0dc55eff9205732a
Deyan Ginev <d.ginev@jacobs-university.de>
2015-09-21 17:54:58 -0400

    adding barebones cortex worker

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+// Copyright 2015 Deyan Ginev. See the LICENSE
+// file at the top-level directory of this distribution.
+//
+// Licensed under the MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>.
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! # The CorTeX library in Rust
+//! The original library can be found at https://github.com/dginev/CorTeX
+
+#![doc(html_root_url = "https://dginev.github.io/rust-cortex-peripherals/")]
+
+extern crate zmq;
+extern crate libc;
+extern crate regex;
+extern crate Archive;
+extern crate rustc_serialize;
+extern crate rand;
+extern crate tempfile;
+
+pub mod worker;
---
+++ tests/echo_worker.rs
@@ -0,0 +1,60 @@
+extern crate pericortex;
+extern crate zmq;
+use std::thread;
+use zmq::{SNDMORE};
+use pericortex::worker::{EchoWorker, Worker};
+
+#[test]
+fn mock_round_trip() {
+  // Let's get a minimal ZMQ ventilator/sink pair to test the worker
+  let test_payload = "cortex peripherals - echo worker test".to_string();
+  let sink_test_payload = test_payload.clone();
+  let vent_thread = thread::spawn(move || {
+    let mut ventilator_context = zmq::Context::new();
+    let mut ventilator = ventilator_context.socket(zmq::ROUTER).unwrap();
+    let ventilator_address = "tcp://*:5555";
+    assert!(ventilator.bind(&ventilator_address).is_ok());
+
+    // We expect one request
+    let mut msg = zmq::Message::new().unwrap();
+    let mut identity = zmq::Message::new().unwrap();
+    ventilator.recv(&mut identity, 0).unwrap();
+    ventilator.recv(&mut msg, 0).unwrap();
+    let service_name = msg.as_str().unwrap().to_string();
+    assert!(service_name == "echo_service");
+
+    ventilator.send_msg(identity, SNDMORE).unwrap();
+    ventilator.send_str("1", SNDMORE).unwrap();
+    ventilator.send_str(&test_payload, 0).unwrap();
+  });
+
+  let sink_thread = thread::spawn(move || {
+    let mut sink_context = zmq::Context::new();
+    let mut sink = sink_context.socket(zmq::PULL).unwrap();
+    let sink_address = "tcp://*:5556";
+    assert!(sink.bind(&sink_address).is_ok());
+
+    let mut service_msg = zmq::Message::new().unwrap();
+    sink.recv(&mut service_msg, 0).unwrap();
+    let service_name = service_msg.as_str().unwrap();
+    assert!(service_name == "echo_service");
+          
+    let mut taskid_msg = zmq::Message::new().unwrap();
+    sink.recv(&mut taskid_msg, 0).unwrap();
+    let taskid_str = taskid_msg.as_str().unwrap();
+    assert!(taskid_str == "1");
+    
+    let mut recv_msg = zmq::Message::new().unwrap();
+    sink.recv(&mut recv_msg, 0).unwrap();
+    let recv_payload = recv_msg.as_str().unwrap();
+    assert!(recv_payload == sink_test_payload);
+  });
+
+  // Start up an echo worker
+  let worker = EchoWorker::default();
+  // Perform a single echo task 
+  assert!(worker.start(Some(1)).is_ok());
+
+  assert!(vent_thread.join().is_ok());
+  assert!(sink_thread.join().is_ok());
+}
