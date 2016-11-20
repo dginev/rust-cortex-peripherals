d4d68d241b87f7ce4acc61055e7cf44088125a85
Deyan Ginev <d.ginev@jacobs-university.de>
2016-11-20 15:17:36 +0200

    and back again, the new 4.1 libzmq requirement in rust-zmq clashes with old Debians

--- tests/echo_worker.rs
+++ tests/echo_worker.rs
@@ -10,7 +10,7 @@ fn mock_round_trip() {
   let test_payload = "cortex peripherals - echo worker test".to_string();
   let sink_test_payload = test_payload.clone();
   let vent_thread = thread::spawn(move || {
-    let ventilator_context = zmq::Context::new();
+    let mut ventilator_context = zmq::Context::new();
     let mut ventilator = ventilator_context.socket(zmq::ROUTER).unwrap();
     let ventilator_address = "tcp://*:5555";
     assert!(ventilator.bind(&ventilator_address).is_ok());
@@ -29,7 +29,7 @@ fn mock_round_trip() {
   });
 
   let sink_thread = thread::spawn(move || {
-    let sink_context = zmq::Context::new();
+    let mut sink_context = zmq::Context::new();
     let mut sink = sink_context.socket(zmq::PULL).unwrap();
     let sink_address = "tcp://*:5556";
     assert!(sink.bind(&sink_address).is_ok());
