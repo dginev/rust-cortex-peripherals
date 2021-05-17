9f5502a1a970203dc7ae9a73d3a024f3bf1e1652
Deyan Ginev <deyan.ginev@gmail.com>
2021-05-17 17:34:00 -0400

    update to preferred .zip method

--- src/adaptor.rs
+++ src/adaptor.rs
@@ -1,12 +1,12 @@
 //! Simple adaptors to relax the CorTeX conentions for agnostic third-party tooling
+use std::error::Error;
 use std::fs::{create_dir_all, File};
+use std::io::copy;
 use std::io::prelude::*;
 use std::io::SeekFrom;
-use std::io::{copy};
 use std::io::{Seek, Write};
 use std::iter::Iterator;
 use std::path::Path;
-use std::error::Error;
 
 use tempdir::TempDir;
 use tempfile::tempfile;
@@ -18,88 +18,88 @@ use zip::ZipArchive;
 /// Transform the ZIP provided by cortex into a TempDir,
 /// for e.g. tools such as Engrafo that aren't ZIP-capable
 pub fn extract_zip_to_tmpdir(path: &Path, tmpdir_prefix: &str) -> Result<TempDir, Box<dyn Error>> {
-  let input_tmpdir = TempDir::new(tmpdir_prefix)?;
-  let unpacked_dir_path = input_tmpdir.path().to_str().unwrap().to_string() + "/";
+    let input_tmpdir = TempDir::new(tmpdir_prefix)?;
+    let unpacked_dir_path = input_tmpdir.path().to_str().unwrap().to_string() + "/";
 
-  // unpack the Zip file for engrafo
-  let inputzip = File::open(&path)?;
-  let mut input_archive = ZipArchive::new(inputzip)?;
-  for i in 0..input_archive.len() {
-    let mut file = input_archive.by_index(i)?;
-    let extract_path = file.sanitized_name();
-    let extract_pathname = extract_path.as_path().display();
-    let full_pathname = format!("{}{}", unpacked_dir_path, extract_pathname);
-    if (&*file.name()).ends_with('/') {
-      create_dir_all(&full_pathname)?;
-    } else {
-      if let Some(p) = extract_path.parent() {
-        if !p.exists() {
-          let absolute_parent = format!("{}{}", unpacked_dir_path, p.display());
-          create_dir_all(&absolute_parent)?;
+    // unpack the Zip file for engrafo
+    let inputzip = File::open(&path)?;
+    let mut input_archive = ZipArchive::new(inputzip)?;
+    for i in 0..input_archive.len() {
+        let mut file = input_archive.by_index(i)?;
+        let extract_path = file.mangled_name();
+        let extract_pathname = extract_path.as_path().display();
+        let full_pathname = format!("{}{}", unpacked_dir_path, extract_pathname);
+        if (&*file.name()).ends_with('/') {
+            create_dir_all(&full_pathname)?;
+        } else {
+            if let Some(p) = extract_path.parent() {
+                if !p.exists() {
+                    let absolute_parent = format!("{}{}", unpacked_dir_path, p.display());
+                    create_dir_all(&absolute_parent)?;
+                }
+            }
+            let mut extracted_file = File::create(&full_pathname)?;
+            copy(&mut file, &mut extracted_file)?;
         }
-      }
-      let mut extracted_file = File::create(&full_pathname)?;
-      copy(&mut file, &mut extracted_file)?;
     }
-  }
-  Ok(input_tmpdir)
+    Ok(input_tmpdir)
 }
 
 /// Adaptor that turns an output temporary directory (assuming the filnema conventions are _already_ ollowed)
 /// into a ZIP file transmittable back to Cortex
 pub fn archive_tmpdir_to_zip(tmpdir: TempDir) -> Result<File, Box<dyn Error>> {
-  let dir_path = tmpdir.path().to_str().unwrap();
-  archive_directory(dir_path)
+    let dir_path = tmpdir.path().to_str().unwrap();
+    archive_directory(dir_path)
 }
 
 const METHOD_DEFLATED: zip::CompressionMethod = zip::CompressionMethod::Deflated;
 
 fn archive_directory(src_dir: &str) -> Result<File, Box<dyn Error>> {
-  let method = METHOD_DEFLATED;
+    let method = METHOD_DEFLATED;
 
-  let mut file = tempfile()?;
+    let mut file = tempfile()?;
 
-  let walkdir = WalkDir::new(src_dir.to_string());
-  let it = walkdir.into_iter();
+    let walkdir = WalkDir::new(src_dir.to_string());
+    let it = walkdir.into_iter();
 
-  zip_one_dir(&mut it.filter_map(Result::ok), src_dir, &mut file, method)?;
+    zip_one_dir(&mut it.filter_map(Result::ok), src_dir, &mut file, method)?;
 
-  file.seek(SeekFrom::Start(0))?;
-  Ok(file)
+    file.seek(SeekFrom::Start(0))?;
+    Ok(file)
 }
 
 fn zip_one_dir<T>(
-  it: &mut dyn Iterator<Item = DirEntry>,
-  prefix: &str,
-  writer: &mut T,
-  method: zip::CompressionMethod,
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &str,
+    writer: &mut T,
+    method: zip::CompressionMethod,
 ) -> zip::result::ZipResult<()>
 where
-  T: Write + Seek,
+    T: Write + Seek,
 {
-  let mut zip = zip::ZipWriter::new(writer);
-  let options = FileOptions::default()
-    .compression_method(method)
-    .unix_permissions(0o755);
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = FileOptions::default()
+        .compression_method(method)
+        .unix_permissions(0o755);
 
-  let mut buffer = Vec::new();
-  for entry in it {
-    let path = entry.path();
-    let name = path
-      .strip_prefix(Path::new(prefix))
-      .unwrap()
-      .to_str()
-      .unwrap();
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path
+            .strip_prefix(Path::new(prefix))
+            .unwrap()
+            .to_str()
+            .unwrap();
 
-    if path.is_file() {
-      zip.start_file(name, options)?;
-      let mut f = File::open(path)?;
+        if path.is_file() {
+            zip.start_file(name, options)?;
+            let mut f = File::open(path)?;
 
-      f.read_to_end(&mut buffer)?;
-      zip.write_all(&*buffer)?;
-      buffer.clear();
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&*buffer)?;
+            buffer.clear();
+        }
     }
-  }
-  zip.finish()?;
-  Result::Ok(())
+    zip.finish()?;
+    Result::Ok(())
 }
